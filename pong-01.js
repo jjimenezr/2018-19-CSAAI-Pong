7de9b12035a76e3182b1533f141690856fd29811
Javier Jimenez Ramiro <jjimenez@beta06.aulas.gsyc.urjc.es>
2019-03-14 11:59:33 +0100

    rebote de la bola

--- pong-01.js
+++ pong-01.js
@@ -14,9 +14,6 @@ function main()
   ctx.fillStyle = 'white';
   ctx.fillRect(500,300,10,40);
 
-  ctx.fillStyle = 'white';
-  ctx.fillRect(125,100,5,5);
-
   var cont = 5;
   while(cont < canvas.height){
     ctx.fillStyle = 'white';
@@ -27,8 +24,74 @@ function main()
   var bolita = {
     x_ini : 125,
     y_ini : 100,
+    x : 0,
+    y : 0,
+    height : 5,
+    width : 5,
+    ctx : null,
+    vx : 5,
+    vy : 2,
+    direction:"right",
 
+    initial: function(ctx){
+      this.ctx = ctx;
+      this.reset();
+    },
 
+    reset: function(){
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    },
+
+    draw: function(){
+      this.ctx.fillStyle = 'white';
+      this.ctx.fillRect(this.x,this.y,this.width,this.height);
+    },
+
+    update: function(){
+      this.x += this.vx;
+      this.y += this.vy;
+    },
+  }
+
+  bolita.initial(ctx)
+  bolita.draw()
+
+  var timer = null;
+  var saque = document.getElementById('saque');
+
+  saque.onclick = () => {
+    if(!timer){
+      timer = setInterval(()=>{
+        bolita.update();
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        bolita.draw();
+
+        if(bolita.x > canvas.width){
+          bolita.vx = -5;
+          bolita.direction = "left";
+        }else if(bolita.y > canvas.height){
+          if(bolita.direction == "right"){
+            bolita.vy = -2;
+            bolita.vx = 5;
+          }else if(bolita.direction == "left"){
+            bolita.vy = -2;
+            bolita.vx = -5;
+          }
+        }else if(bolita.x < 0){
+          bolita.vx = 5;
+          bolita.direction = "right";
+        }else if(bolita.y < 0){
+          if(bolita.direction == "right"){
+            bolita.vy = 2;
+            bolita.vx = 5;
+          }else if(bolita.direction == "left"){
+            bolita.vy = 2;
+            bolita.vx = -5;
+          }
+        }
+      },15);
+    }
   }
 
 }

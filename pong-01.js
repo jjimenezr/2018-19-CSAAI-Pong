0f48b5c4bc028d730ec7dee25e949ee12b6dfe14
Javier Jimenez Ramiro <jjimenez@beta06.aulas.gsyc.urjc.es>
2019-03-21 11:28:24 +0100

    la raqueta 1 se mueve

--- pong-01.js
+++ pong-01.js
@@ -9,21 +9,20 @@ function main()
   var ctx= canvas.getContext("2d");
 
   ctx.fillStyle = 'white';
-  ctx.fillRect(50,25,10,40);
-
-  ctx.fillStyle = 'white';
   ctx.fillRect(500,300,10,40);
 
-  var cont = 5;
-  while(cont < canvas.height){
-    ctx.fillStyle = 'white';
-    ctx.fillRect(canvas.width/2,cont,2,10);
-    cont = cont +20;
+  function drawline() {
+    var cont = 5;
+    while(cont < canvas.height){
+      ctx.fillStyle = 'white';
+      ctx.fillRect(canvas.width/2,cont,2,10);
+      cont = cont +20;
+    }
   }
 
   var bolita = {
-    x_ini : 125,
-    y_ini : 100,
+    x_ini : 300,
+    y_ini : 200,
     x : 0,
     y : 0,
     height : 5,
@@ -54,8 +53,47 @@ function main()
     },
   }
 
+  var raquet1 = {
+    x_ini : 40,
+    y_ini : 25,
+    x : 0,
+    y : 0,
+    height : 40,
+    width : 10,
+    ctx : null,
+    vy : 5,
+    direction : null,
+
+    initial: function(ctx){
+      this.ctx = ctx;
+      this.reset();
+    },
+
+    reset: function(){
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    },
+
+    draw: function(){
+      this.ctx.fillStyle = 'white';
+      this.ctx.fillRect(this.x,this.y,this.width,this.height);
+    },
+
+    update: function(){
+      if(this.direction == "up"){
+        this.y = this.y - this.vy;
+      }else if(this.direction == "down"){
+        this.y += this.vy;
+      }
+    },
+  }
+
   bolita.initial(ctx)
   bolita.draw()
+  raquet1.initial(ctx)
+  raquet1.draw()
+  drawline()
+
 
   var timer = null;
   var saque = document.getElementById('saque');
@@ -64,8 +102,33 @@ function main()
     if(!timer){
       timer = setInterval(()=>{
         bolita.update();
+        raquet1.update();
         ctx.clearRect(0,0,canvas.width,canvas.height);
         bolita.draw();
+        raquet1.draw();
+        drawline();
+
+        window.onkeydown = (e) => {
+
+          e.preventDefault();
+          if(e.key == 'w'){
+            raquet1.direction = "up";
+          }else if(e.key == 's'){
+            raquet1.direction = "down";
+          }
+        }
+
+        window.onkeyup = (e) => {
+          raquet1.direction = "quieta";
+        }
+
+        if(raquet1.y + raquet1.height >= canvas.height){
+          raquet1.direction = "quieta";
+        }else if (raquet1.y <= 0){
+          raquet1.direction = "quieta";
+        }
+
+        
 
         if(bolita.x > canvas.width){
           bolita.vx = -5;
